182b3c7693eb0262d50462771aea30cc36584137
BriAnna Slik <daffadelicious@gmail.com>
2024-01-16 00:24:43 -0700

    testing rename

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("nodemon");
+    res.send("Test");
 })
 
 app.listen(process.env.PORT || 3000, () => {
